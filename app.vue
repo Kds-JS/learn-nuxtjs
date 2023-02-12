d9c5d7f97bacbc787dd30a8aa9188310c50a029a
kds <kdskoudous98@gmail.com>
2023-02-12 09:12:41 +0100

    lesson-3

--- app.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <!-- <NuxtWelcome /> -->
-    <h1>Hello world</h1>
-  </div>
-</template>
---
+++ pages/about.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati
+      commodi ex laudantium beatae, quaerat ut, repudiandae, similique quos nam
+      sapiente culpa doloremque neque. Dolores, suscipit reprehenderit
+      consequuntur asperiores maiores tempora!
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati
+      commodi ex laudantium beatae, quaerat ut, repudiandae, similique quos nam
+      sapiente culpa doloremque neque. Dolores, suscipit reprehenderit
+      consequuntur asperiores maiores tempora!
+    </p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
