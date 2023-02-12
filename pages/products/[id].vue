af85a23fd8a202f51860b001d79393112f809f19
kds <kdskoudous98@gmail.com>
2023-02-12 09:23:33 +0100

    add route parameters

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus similique sunt eum veritatis aut possimus autem, unde vitae sapiente explicabo consequatur doloremque dicta eos, temporibus eveniet commodi error expedita dolores.</p>
+    </div>
+</template>
+
+<script setup>
+    const {id} = useRoute().params;
+</script>
+
+<style scoped>
+
+</style>
